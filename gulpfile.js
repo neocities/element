cd0f489c2eca279b8a0231da26e506d9c163463c
Kyle Drake <kyle@kyledrake.net>
2017-05-09 13:53:13 -0700

    more tweaks to css, version based dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,18 @@ var gulp = require('gulp')
 var uglify = require('gulp-uglify')
 var pump = require('pump')
 var cleanCSS = require('gulp-clean-css')
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
+var concat = require('gulp-concat')
+var rename = require('gulp-rename')
+var fs = require('fs')
+
+var pkg = JSON.parse(fs.readFileSync('./package.json'))
 
 gulp.task('minifyjs', function (cb) {
   pump([
         gulp.src('element.js'),
         uglify(),
-        concat('element.min.js'),
-        gulp.dest('.')
+        concat('element-'+pkg.version+'.min.js'),
+        gulp.dest('dist')
     ],
     cb
   )
@@ -19,8 +22,8 @@ gulp.task('minifyjs', function (cb) {
 gulp.task('minifycss', function() {
   return gulp.src('element.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(concat('element.min.css'))
-    .pipe(gulp.dest('.'))
+    .pipe(concat('element-'+pkg.version+'.min.css'))
+    .pipe(gulp.dest('dist'))
 })
 
 gulp.task('default', ['minifyjs', 'minifycss']);
